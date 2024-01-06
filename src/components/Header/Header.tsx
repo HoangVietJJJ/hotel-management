bd549b29e9a7fc65b84031606640446eada1ca88
HoangViet <hoangphamhl000@gmail.com>
2024-01-06 22:47:18 +0700

    Update 1

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,17 @@
+'use client';
+
+import ThemeContext from "@/context/themeContext";
 import Link from "next/link";
+import { useContext } from "react";
 import { FaUserCircle } from "react-icons/fa";
-import { MdDarkMode } from "react-icons/md";
+import { MdDarkMode, MdOutlineLightMode } from "react-icons/md";
 
 const Header = () => {
+    const { darkTheme, setDarkTheme } = useContext(ThemeContext)
+
     return <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
         <div className="flex items-center w-full md:2/3">
-            <Link href="/" className="font-black text-[#F27405]">
+            <Link href="/" className="font-black text-tertiary-dark">
                 Fantasticzzz
             </Link>
             <ul className="flex items-center ml-5">
@@ -14,8 +20,21 @@ const Header = () => {
                         <FaUserCircle className="cursor-pointer" />
                     </Link>
                 </li>
-                <li className="ml-2">
-                    <MdDarkMode className="cursor-poiter" />
+                <li className="ml-3">
+                    {darkTheme ? (<MdOutlineLightMode
+                        className="cursor-pointer"
+                        onClick={() => {
+                            setDarkTheme(false)
+                            localStorage.removeItem('hotel-theme')
+                        }}
+                    />) :
+                        (<MdDarkMode
+                            className="cursor-pointer"
+                            onClick={() => {
+                                setDarkTheme(true)
+                                localStorage.setItem('hotel-theme', 'true')
+                            }}
+                        />)}
                 </li>
             </ul>
         </div>
